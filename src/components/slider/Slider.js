e969b25062a5d60c6b4b54681c3ccd24d2c32657
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2022-08-16 11:09:50 +0300

    fix: added style for slider and react-icons packet

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper';
 import { graphql, useStaticQuery } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 //
 import { useTranslation } from 'react-i18next';
+import { BsArrowRightCircle, BsArrowLeftCircle } from 'react-icons/bs';
+//
 
 import 'swiper/css';
 import 'swiper/css/navigation';
+//
 
 const Slider = ({ images }) => {
   const { i18n } = useTranslation();
@@ -34,28 +37,31 @@ const Slider = ({ images }) => {
   const mdxData = data.allMdx.nodes;
   console.log('mdxData', mdxData);
 
-  // const swiper = new Swiper('.swiper', {
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   },
-  // });
+  const swiperNavPrevRefs = useRef(null);
+  const swiperNavNextRefs = useRef(null);
 
   return (
-    <>
+    <div className="relative">
       <Swiper
+        className="w-4/5"
         modules={[Navigation]}
-        spaceBetween={20}
-        slidesPerView={3}
+        spaceBetween={10}
+        slidesPerView={2}
         onSlideChange={() => console.log('slide change')}
         onSwiper={swiper => console.log('test>>', swiper)}
-        navigation={true}
+        navigation={{
+          prevEl: swiperNavPrevRefs.current,
+          nextEl: swiperNavNextRefs.current,
+        }}
         pagination={{ clickable: true }}
       >
         {mdxData.map(({ frontmatter }) => {
           console.log(frontmatter);
           return (
-            <SwiperSlide key={frontmatter.id}>
+            <SwiperSlide
+              key={frontmatter.title}
+              className="border-2 border-orange-600 text-center"
+            >
               <h3>{frontmatter.title}</h3>
               <p>{frontmatter.date}</p>
               <p>{frontmatter[i18n.language]}</p>
@@ -66,13 +72,23 @@ const Slider = ({ images }) => {
           const imageData = item.cloudinaryImg.childImageSharp.gatsbyImageData;
           const imageAlt = item.frontmatter[i18n.language];
           return (
-            <SwiperSlide key={item.id}>
-              <GatsbyImage image={imageData} alt={imageAlt} />
+            <SwiperSlide key={item.id} className="border-2 border-orange-600 ">
+              <GatsbyImage
+                image={imageData}
+                alt={imageAlt}
+                className="my-0 mx-auto"
+              />
             </SwiperSlide>
           );
         })}
       </Swiper>
-    </>
+      <div className="swiper-button-prev" ref={swiperNavPrevRefs}>
+        <BsArrowLeftCircle className="w-12 h-12 fill-orange-600" />
+      </div>
+      <div className="swiper-button-next" ref={swiperNavNextRefs}>
+        <BsArrowRightCircle className="w-12 h-12 fill-orange-600" />
+      </div>
+    </div>
   );
 };
 export default Slider;
