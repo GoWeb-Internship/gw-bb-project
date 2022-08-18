0844b9a80671e17476d3398046a79cb17d40795f
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2022-08-18 15:44:53 +0300

    fix: key for list section hero

--- src/components/hero/HeroDataList.js
+++ src/components/hero/HeroDataList.js
@@ -7,8 +7,8 @@ const HeroDataList = ({ heroDataList }) => {
         {heroDataList.title}:
       </h2>
       <ul className="grid gap-x-10 gap-y-10 relative mb-9 md:grid-cols-2 lg:grid-cols-3">
-        {heroDataList.list.map(item => (
-          <li key={item} className="border-t border-slate-50 py-6">
+        {heroDataList.list.map((item, id) => (
+          <li key={`hero-${id}`} className="border-t border-slate-50 py-6">
             <p className="text-lg leading-[25px]">{item}</p>
           </li>
         ))}
