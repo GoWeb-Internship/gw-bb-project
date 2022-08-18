e7a90ac1f5f821373c0cc9c9fceaf83473fdd8a6
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2022-08-18 23:43:03 +0300

    fix: style

--- src/components/myFormulaSection/MyFormulaSection.js
+++ src/components/myFormulaSection/MyFormulaSection.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
 import MyFormulaList from './MyFormulaList';
 import Section from '../reusable/Section';
-import Container from '../reusable/Container';
 
 const MyFormulaSection = () => {
   const { t } = useTranslation();
@@ -12,12 +11,10 @@ const MyFormulaSection = () => {
 
   return (
     <Section>
-      {/* <Container className="-px-20"> */}
       <div className="bg-orange-300 mx-auto my-0 h-full pt-[124px] pb-[64px] px-[190px] text-slate-50 lg:w-[1440px]">
         <h2 className="mb-14 text-center">{myFormula.title}</h2>
         <MyFormulaList data={myFormula} />
       </div>
-      {/* </Container> */}
     </Section>
   );
 };
