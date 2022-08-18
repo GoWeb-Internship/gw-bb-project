267d036f4b002611e34f196de018e52d7d74d051
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2022-08-18 16:41:05 +0300

    feat: structure and style for section be better

---
+++ src/components/beBetterToday/BeBetterToday.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useTranslation } from 'gatsby-plugin-react-i18next';
+import { graphql, useStaticQuery } from 'gatsby';
+
+import Section from '../reusable/Section';
+import Background from '../reusable/Background';
+import Container from '../reusable/Container';
+import List from 'components/reusable/List';
+
+const BeBetterToday = () => {
+  const { t } = useTranslation();
+  const beBetterToday = t('beBetterToday', {
+    returnObjects: true,
+  });
+
+  const imageData = useStaticQuery(graphql`
+    query {
+      bg: file(name: { eq: "fon-be-better-today" }) {
+        id
+        publicURL
+        childImageSharp {
+          id
+          gatsbyImageData
+        }
+      }
+    }
+  `);
+
+  return (
+    <Section>
+      <Background imageData={imageData.bg} />
+      <Container>
+        <div className="lg:pt-[124px] lg:pb-[68px]">
+          <h2 className="text-orange-400 pb-[46px]">
+            {beBetterToday.title}
+            <span className="block">{beBetterToday.subTitle}</span>
+          </h2>
+          <p className="text-neutral-600 font-bold leading-10 mb-2">
+            {beBetterToday.textSupItems}:
+          </p>
+          <List items={beBetterToday.items} />
+          <p className="text-neutral-600 font-bold leading-10 mt-3">
+            {beBetterToday.textSubItems}
+          </p>
+        </div>
+      </Container>
+    </Section>
+  );
+};
+
+export default BeBetterToday;
