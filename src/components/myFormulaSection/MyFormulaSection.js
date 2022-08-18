19d088c5fa2ec26ca682c3eee0ac74a42ba47924
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2022-08-18 23:40:17 +0300

    fix: corrected style section my formula

--- src/components/myFormulaSection/MyFormulaSection.js
+++ src/components/myFormulaSection/MyFormulaSection.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
 import MyFormulaList from './MyFormulaList';
+import Section from '../reusable/Section';
+import Container from '../reusable/Container';
 
 const MyFormulaSection = () => {
   const { t } = useTranslation();
@@ -9,12 +11,14 @@ const MyFormulaSection = () => {
   });
 
   return (
-    <>
-      <div className="bg-orange-300 w-full h-full pt-[124px] pb-[64px] px-[190px] text-slate-50">
+    <Section>
+      {/* <Container className="-px-20"> */}
+      <div className="bg-orange-300 mx-auto my-0 h-full pt-[124px] pb-[64px] px-[190px] text-slate-50 lg:w-[1440px]">
         <h2 className="mb-14 text-center">{myFormula.title}</h2>
         <MyFormulaList data={myFormula} />
       </div>
-    </>
+      {/* </Container> */}
+    </Section>
   );
 };
 
