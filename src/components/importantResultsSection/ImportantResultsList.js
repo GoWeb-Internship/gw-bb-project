ddd22c411c71d223f564f317bd747b0619dc0b32
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2022-08-19 22:57:32 +0300

    feat: add style secrion 6 results for mobile

--- src/components/importantResultsSection/ImportantResultsList.js
+++ src/components/importantResultsSection/ImportantResultsList.js
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types';
 
 const ImportantResultsList = ({ data }) => {
   return (
-    <ul className="grid lg:grid-cols-3 md:grid-cols-2 md:gap-x-6 lg:gap-x-20 lg:gap-y-[60px] md:gap-y-9">
+    <ul className="grid gap-y-4 md:grid-cols-2 md:gap-x-6 md:gap-y-9 lg:grid-cols-3 lg:gap-x-20 lg:gap-y-[60px]">
       {data.list.map(item => (
         <li
           key={item}
-          className="lg:w-[360px] md:w-[338px] md:h-[186px] lg:h-[186px] border-slate-50 py-7 lg:px-[35px] md:px-8 border rounded-[20px]"
+          className="w-[280px] h-[172px] border-slate-50 py-7 px-6 border rounded-[20px] md:w-[338px] md:h-[186px] md:px-8 lg:w-[360px] lg:h-[186px] lg:px-[35px]"
         >
-          <p className="text-bb1625 font-bold">{item}</p>
+          <p className="text-bb1225 font-bold  md:text-bb1625 lg:text-bb1625">
+            {item}
+          </p>
         </li>
       ))}
     </ul>
