f90947b81d096aec83fc58145fee224171c059aa
Pavel Sheremet <pashqa.sheremet@gmail.com>
2022-08-20 15:20:25 +0300

    ref: styles for stories and feedbacksection

--- src/components/reusable/Slider.js
+++ src/components/reusable/Slider.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { BsArrowRightCircle, BsArrowLeftCircle } from 'react-icons/bs';
 import { Swiper } from 'swiper/react';
 import { Navigation } from 'swiper';
+import { PageFormatContext } from 'context/PageFormatContext';
 
 const Slider = ({
   children,
@@ -11,10 +12,14 @@ const Slider = ({
   onSlideChange,
   onSwiper,
 }) => {
+  const pageFormat = useContext(PageFormatContext);
+
+  const isMobile = pageFormat === 'mobile';
+
   return (
-    <div className={`relative ${className}`}>
+    <div className={`relative  ${className}`}>
       <Swiper
-        className={`max-w-[1100px]`}
+        className={`mx-auto slider-width sm:max-w-[420px] md:max-w-[550px] lg:max-w-[1100px]`}
         modules={[Navigation]}
         slidesPerView={slidesPerView}
         onSlideChange={onSlideChange}
@@ -23,16 +28,21 @@ const Slider = ({
           nextEl: '.next-slider',
           prevEl: '.prev-slider',
         }}
+        loop={true}
       >
         {children}
       </Swiper>
 
-      <div className="prev-slider swiper-button-disabled" role={'button'}>
-        <BsArrowLeftCircle size={40} />
-      </div>
-      <div className="next-slider swiper-button-disabled" role={'button'}>
-        <BsArrowRightCircle size={40} />
-      </div>
+      {!isMobile && (
+        <>
+          <div className="prev-slider swiper-button-disabled" role={'button'}>
+            <BsArrowLeftCircle size={40} />
+          </div>
+          <div className="next-slider swiper-button-disabled" role={'button'}>
+            <BsArrowRightCircle size={40} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
