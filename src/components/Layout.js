2e1a70f9be64cfbd7ee9bfeda9716a90f0413cc6
Pavel Sheremet <pashqa.sheremet@gmail.com>
2022-08-20 09:17:51 +0300

    ref: change ClientLocation hook, add custom useMedia Hook, add Contexts for location and media

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,15 +10,34 @@ import PropTypes from 'prop-types';
 import Header from './header/Header';
 import Main from './main/Main';
 import Footer from './footer/Footer';
+import useMedia from 'hooks/useMedia';
+import { getMediaQueries } from 'styles/vars';
+import { PageFormatContext } from 'context/PageFormatContext';
+import useClientLocation from 'hooks/useClientLocation';
+import { ClientLoactionContext } from 'context/ClientLoactionContext';
+
+const mediaQueries = getMediaQueries();
 
 const Layout = ({ children, saleText, charity }) => {
+  const pageFormat = useMedia(
+    Object.values(mediaQueries),
+    Object.keys(mediaQueries),
+    'mobile',
+  );
+
+  const clientLocation = useClientLocation();
+
   return (
     <>
-      <Header />
-      <div>
-        <Main>{children}</Main>
-        <Footer saleText={saleText} charity={charity} />
-      </div>
+      <ClientLoactionContext.Provider value={clientLocation}>
+        <PageFormatContext.Provider value={pageFormat}>
+          <Header />
+          <div>
+            <Main>{children}</Main>
+            <Footer saleText={saleText} charity={charity} />
+          </div>
+        </PageFormatContext.Provider>
+      </ClientLoactionContext.Provider>
     </>
   );
 };
