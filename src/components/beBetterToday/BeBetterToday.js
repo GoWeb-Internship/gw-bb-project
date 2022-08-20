3e2e15035c12bcf8b590bfb03245ef33e01ff4d9
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2022-08-21 01:09:56 +0300

    feat: add style mob, tablet  section be better

--- src/components/beBetterToday/BeBetterToday.js
+++ src/components/beBetterToday/BeBetterToday.js
@@ -30,16 +30,16 @@ const BeBetterToday = () => {
     <Section>
       <Background imageData={imageData.bg} />
       <Container>
-        <div className="lg:pt-[124px] lg:pb-[68px]">
-          <h2 className="text-orange-400 pb-[46px]">
+        <div className="pt-9 pb-[72px] md:py-20 lg:pt-[124px] lg:pb-[68px]">
+          <h2 className="text-orange-400 mb-6 md:mb-[46px] md:text-center lg:text-start">
             {beBetterToday.title}
             <span className="block">{beBetterToday.subTitle}</span>
           </h2>
-          <p className="text-neutral-600 font-bold leading-10 mb-2">
+          <p className="text-neutral-600 text-[14px] font-bold leading-10 mb-2 md:text-[20px] ">
             {beBetterToday.textSupItems}:
           </p>
           <List items={beBetterToday.items} />
-          <p className="text-neutral-600 font-bold leading-10 mt-3">
+          <p className="text-neutral-600 font-bold text-[14px] leading-[22px] md:text-[20px] md:leading-10 mt-3 md:mt-5 md:text-center lg:text-start lg:mt-3">
             {beBetterToday.textSubItems}
           </p>
         </div>
