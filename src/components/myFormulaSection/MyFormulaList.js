6a3c24ca1ed4092619e611024edffda30efc105e
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2022-08-20 23:55:41 +0300

    feat: add tab and mob style for formula section

--- src/components/myFormulaSection/MyFormulaList.js
+++ src/components/myFormulaSection/MyFormulaList.js
@@ -6,18 +6,24 @@ import LampWithArrowIcon from 'images/lampWithArrow.inline.svg';
 
 const MyFormulaList = ({ data }) => {
   return (
-    <ul className="grid grid-rows-3 grid-flow-col gap-x-[156px]">
-      <li className="row-start-2 row-span-2 text-center">
-        <LampIcon className="hover:scale-125 pb-8" />
-        <p className="leading-10 font-bold">{data.textUnderstand}</p>
+    <ul className="grid grid-rows-3 grid-flow-col md:gap-x-16 lg:gap-x-[156px]">
+      <li className="text-center md:row-end-3 md:row-span-2 lg:row-start-2 lg:row-span-2">
+        <LampIcon className="duration-200 transition-transform hover:scale-125 w-[108px] mb-3.5 md:w-full md:pb-6 lg:pb-8" />
+        <p className="leading-10 text-[16px] font-bold md:text-[20px]">
+          {data.textUnderstand}
+        </p>
       </li>
-      <li className="row-end-3 row-span-2 text-center">
-        <LampInHandIcon className="hover:scale-125 pb-8" />
-        <p className="leading-10 font-bold">{data.textHowGet}</p>
+      <li className="text-center md:row-start-2 md:row-span-2 lg:row-end-3 lg:row-span-2">
+        <LampInHandIcon className="duration-200 transition-transform hover:scale-125 w-[108px] mb-3.5 md:w-full md:pb-6 lg:pb-8" />
+        <p className="leading-10 text-[16px] font-bold md:text-[20px]">
+          {data.textHowGet}
+        </p>
       </li>
-      <li className="row-start-2 row-span-2 text-center">
-        <LampWithArrowIcon className="hover:scale-125 pb-8" />
-        <p className="leading-10 font-bold">{data.textStartDoing}</p>
+      <li className="text-center md:row-end-3 md:row-span-2 lg:row-start-2 lg:row-span-2">
+        <LampWithArrowIcon className="duration-200 transition-transform hover:scale-125 w-[108px] mb-3.5 md:w-full md:pb-6 lg:pb-8" />
+        <p className="leading-10 text-[16px] font-bold md:text-[20px]">
+          {data.textStartDoing}
+        </p>
       </li>
     </ul>
   );
