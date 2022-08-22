fec924dd8e539d831a0649128809fda273d131e2
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2022-08-22 17:08:44 +0300

    add compressed picture

--- src/components/importantResultsSection/ImportantResultsSection.js
+++ src/components/importantResultsSection/ImportantResultsSection.js
@@ -15,7 +15,7 @@ const ImportantResultsSection = () => {
 
   const imageData = useStaticQuery(graphql`
     query {
-      bg: file(name: { eq: "fon-six-main-results-2" }) {
+      bg: file(name: { eq: "fon-six-main-results-2-1" }) {
         id
         publicURL
         childImageSharp {
