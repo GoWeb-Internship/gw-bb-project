709fcdb8090f45d7841ed1f734fcd09232953a57
SvitlanaZhovanik <Lanasvetcat@gmail.com>
2022-08-22 21:12:03 +0300

    test renamed fon picture

--- src/components/importantResultsSection/ImportantResultsSection.js
+++ src/components/importantResultsSection/ImportantResultsSection.js
@@ -15,7 +15,7 @@ const ImportantResultsSection = () => {
 
   const imageData = useStaticQuery(graphql`
     query {
-      bg: file(name: { eq: "fon-six-main-results-2" }) {
+      bg: file(name: { eq: "fon-six-results" }) {
         id
         publicURL
         childImageSharp {
