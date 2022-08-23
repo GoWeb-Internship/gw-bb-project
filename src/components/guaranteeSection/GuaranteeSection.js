b1e06f241285e77f65794eb8b93441ae6a0be966
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2022-08-23 16:06:13 +0300

    fix: add styles for tab and mobile, bck img

--- src/components/guaranteeSection/GuaranteeSection.js
+++ src/components/guaranteeSection/GuaranteeSection.js
@@ -1,29 +1,61 @@
-import * as React from 'react';
+import React, { useContext } from 'react';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
+import { PageFormatContext } from 'context/PageFormatContext';
+import { graphql, useStaticQuery } from 'gatsby';
 
 import Section from '../reusable/Section';
+import Background2 from 'components/reusable/Background2';
+import Container from '../reusable/Container';
 import video from '../../video/sea.mp4';
 
 const GuaranteeSection = () => {
+  const pageFormat = useContext(PageFormatContext);
   const { t } = useTranslation();
   const guarantee = t('guarantee', { returnObjects: true });
 
+  const imageData = useStaticQuery(graphql`
+    query {
+      bg: file(name: { eq: "fon-guarantee" }) {
+        id
+        publicURL
+        childImageSharp {
+          id
+          gatsbyImageData
+        }
+      }
+    }
+  `);
+
   return (
     <Section>
-      <div className="lg:max-h-[540px] overflow-y-hidden md:max-h-[382px]">
-        <video
-          className="relative my-0 mx-auto lg:max-w-[1440px] md:max-w-[768px]"
-          autoPlay={true}
-          muted
-          loop
-        >
-          <source src={video} type="video/mp4" />
-        </video>
-        <div className="absolute lg:left-[50%] lg:top-1/3 origin-top-left text-slate-50 md:top-[18%] md:left-[40%]">
-          <h2 className="mb-12">{guarantee.title}</h2>
-          <p className="lg:w-[620px] md:w-[424px]">{guarantee.text}</p>
+      {pageFormat === 'desktop' ? (
+        <div className="lg:max-h-[540px] overflow-y-hidden md:max-h-[382px]">
+          <video
+            className="relative my-0 mx-auto lg:max-w-[1440px] md:max-w-[768px]"
+            autoPlay={true}
+            muted
+            loop
+          >
+            <source src={video} type="video/mp4" />
+          </video>
+          <div className="lg:absolute lg:left-[50%] lg:top-1/3 lg:origin-top-left lg:text-slate-50">
+            <h2 className="mb-12">{guarantee.title}</h2>
+            <p className="lg:w-[620px] md:w-[424px]">{guarantee.text}</p>
+          </div>
         </div>
-      </div>
+      ) : (
+        <>
+          <Background2 imageData={imageData.bg} />
+          <Container>
+            <div className="pt-9 pb-28 md:pt-20 md:pb-[90px] md:text-center">
+              <h2 className="mb-6 text-center md:mb-12">{guarantee.title}</h2>
+              <p className="sm:text-center md:text-bb2030 md:tracking-[0.02]">
+                {guarantee.text}
+              </p>
+            </div>
+          </Container>
+        </>
+      )}
     </Section>
   );
 };
