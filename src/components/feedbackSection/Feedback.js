7117d3e6b60837af11b839b64912c784d12c2fbb
SvitlanaZhovanik <Lanasvetcat@gmail.com>
2022-08-26 15:46:08 +0300

    test 4 added style flex relative z-10

--- src/components/feedbackSection/Feedback.js
+++ src/components/feedbackSection/Feedback.js
@@ -16,8 +16,8 @@ const Feedback = ({
             <GatsbyImage
               image={image}
               alt={content[`${i18n.language}Name`]}
-              className="max-w-full max-h-full rounded-full overflow-hidden"
-              imgClassName="rounded-full overflow-hidden"
+              className="max-w-full max-h-full rounded-full overflow-hidden relative z-10"
+              imgClassName="rounded-full overflow-hidden flex"
             />
           )}
         </div>
