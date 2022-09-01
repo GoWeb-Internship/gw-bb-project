37da1eb3541c0db4437b585288d04a3e9424063b
Pavel Sheremet <pashqa.sheremet@gmail.com>
2022-09-01 16:08:31 +0300

    ref: connect fonts localy

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
+import stylesheet from 'assets/fonts/stylesheet.css';
 
 import image from 'images/seo.png';
 
@@ -71,10 +72,7 @@ const Seo = ({ description = '', lang = 'uk', meta = [], title }) => {
       <title>{title}</title>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Ubuntu:wght@500;700&display=swap"
-        rel="stylesheet"
-      />
+      <link href={stylesheet} rel="stylesheet" />
     </Helmet>
   );
 };
